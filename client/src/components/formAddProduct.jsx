da5e2a085629b6402f2c29ce6aa8115a6febeba5
WandaCirone <wandacirone@gmail.com>
2020-09-10 14:25:47 -0300

    Formulario con fetch

--- client/src/components/formAddProduct.jsx
+++ client/src/components/formAddProduct.jsx
@@ -8,20 +8,53 @@ export default class formAddProduct extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            titulo: "",
-            categoria: "",
-            precio: "",
-            cantidad: "",
-            descripcion: "",
+            title: "",
+            categories: "",
+            price: "",
+            quantity: "",
+            description: "",
             imagenes: []
         }
 
         this.handleChange = this.handleChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     handleChange(e) {
         this.setState({[e.target.name]: e.target.value})
     }
+    onSubmit(product) {
+      // if(!product.title || !product.categories || !product.price || !product.quantity || !product.description) {
+      //   return alert("Todos los campos deben estar completos")
+      // }
+      // else {
+        var url = `http://localhost:3001/products`;
+        var data =  {
+        title: product.title,
+        categories: product.categories, 
+        price: product.price,
+        quantity: product.quantity
+        };
+        fetch(url, {
+        method: 'POST', 
+        body: JSON.stringify(data),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+        })
+        .then(res => res.json())
+        .then(()=>alert("El formulario ha sido enviado exitosamente"))
+        .catch(error => console.error('Error:', error))
+       
+        this.setState ({ 
+          title: "",
+          categories: "",
+          price: "",
+          quantity: "",
+          description: "",
+        })
+      }
+  //}
 
     render () { 
     return( 
@@ -32,6 +65,8 @@ export default class formAddProduct extends React.Component {
               id="outlined-basic" 
               label="Título" 
               variant="outlined" 
+              name= "title"
+              value={this.state.title} 
               onChange = {this.handleChange}
               /> <br/> <br/>
 
@@ -40,6 +75,9 @@ export default class formAddProduct extends React.Component {
               id="outlined-basic" 
               label="Categoría" 
               variant="outlined" 
+              name= "categories"
+              value={this.state.categories} 
+              onChange = {this.handleChange}
               /> <br/> <br/>
 
               <TextField
@@ -50,6 +88,8 @@ export default class formAddProduct extends React.Component {
               shrink: true,
               }}
               variant="outlined"
+              name= "price"
+              value={this.state.price} 
               onChange = {this.handleChange}
               /> <br/> <br/>
 
@@ -61,6 +101,9 @@ export default class formAddProduct extends React.Component {
               shrink: true,
               }}
               variant="outlined"
+              name= "quantity"
+              value={this.state.quantity} 
+              onChange = {this.handleChange}
               /> <br/> <br/>
 
               <TextField 
@@ -70,9 +113,12 @@ export default class formAddProduct extends React.Component {
               multiline
               rows={4}
               variant="outlined"
+              name= "description"
+              value={this.state.description} 
+              onChange = {this.handleChange}
               /> <br/> <br/>
 
-              <Button variant="contained" color="primary">
+              <Button variant="contained" color="primary" onClick={this.onSubmit} >
                 Enviar
               </Button>
 
