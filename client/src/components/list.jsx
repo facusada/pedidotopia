1919a2d7eb0617cb70462d04af3db78d840fffc3
WandaCirone <wandacirone@gmail.com>
2020-09-13 20:41:26 -0300

    Cambio lista a componente de Clase

--- client/src/components/list.jsx
+++ client/src/components/list.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react'; 
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react'; 
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,59 +6,67 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/styles';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
-
-
-export default function SimpleTable()  {
-
-   const classes = useStyles();
-   const [productos, setProductos] = React.useState(null);
-
-    useEffect(() => { 
-    var url = `http://localhost:3000/products`
-    fetch(url, {
-      method: 'GET',
-    })
-    .then(res => res.json())
-    .then((productos) => {
-      setProductos(productos)
-    })
-    .catch(error => console.error('Error:', error))
-    })
-  
-
-    return (
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell align="right">Nombre</TableCell>
-              <TableCell align="right">Precio</TableCell>
-              <TableCell align="right">Cantidad</TableCell>
-              <TableCell align="right">Descripción</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {/* {productos.map((p) => (
-              <TableRow key={p.name}>
-                <TableCell component="th" scope="row">
-                  {p.id}
-                </TableCell>
-                <TableCell align="right">{p.title}</TableCell>
-                <TableCell align="right">{p.price}</TableCell>
-                <TableCell align="right">{p.quantity}</TableCell>
-                <TableCell align="right">{p.description}</TableCell>
-              </TableRow>
-            ))} */}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    )
-}
  
+const styles = theme => ({
+    table: {
+      minWidth: 650,
+    },
+  });
+
+ class SimpleTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            prod: ""
+        }
+    }
+
+    componentDidMount() { 
+        var url = `http://localhost:3000/products`
+        fetch(url, {
+          method: 'GET',
+        })
+        .then(res => res.json())
+        .then((productos) => {
+            console.log(productos)
+          this.setState({prod:productos})
+        })
+        .catch(error => console.error('Error:', error))
+        }
+    
+    render(){
+        const { classes } = this.props;
+        return (
+            <TableContainer component={Paper}>
+              <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>ID</TableCell>
+                    <TableCell align="right">Nombre</TableCell>
+                    <TableCell align="right">Precio</TableCell>
+                    <TableCell align="right">Cantidad</TableCell>
+                    <TableCell align="right">Descripción</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {/* {productos.map((p) => (
+                    <TableRow key={p.name}>
+                      <TableCell component="th" scope="row">
+                        {p.id}
+                      </TableCell>
+                      <TableCell align="right">{p.title}</TableCell>
+                      <TableCell align="right">{p.price}</TableCell>
+                      <TableCell align="right">{p.quantity}</TableCell>
+                      <TableCell align="right">{p.description}</TableCell>
+                    </TableRow>
+                  ))} */}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )
+    }
+}
+
+export default withStyles(styles, { withTheme: true })(SimpleTable);
