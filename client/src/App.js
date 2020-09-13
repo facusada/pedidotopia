a67e5338be9a8de69ed22a866935907b8c2dbb16
WandaCirone <wandacirone@gmail.com>
2020-09-13 18:30:10 -0300

    Dropdown menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 import Form from './components/formAddProduct';
 import NavBar from './components/NavBar';
+import List from './components/list';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Form/>
+      <Route
+      path='/'
+      component={NavBar}
+      />
+      <Route
+      exact path='/addProduct'
+      component={Form}
+      /> 
+      <Route
+      exact path='/list'
+      component={List}
+      />
     </div>
   );
 }
