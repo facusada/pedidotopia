1eebc95042e84ea092070e9c8820de184f943ab1
brunodionel <brunovicente32@gmail.com>
2020-09-18 05:59:51 -0300

    alerta no productos

--- client/src/components/list.jsx
+++ client/src/components/list.jsx
@@ -17,20 +17,20 @@ const image = {
 }
 
 const styles = theme => ({
-    table: {
-      minWidth: 650,
-    },
-  });
+  table: {
+    minWidth: 650,
+  },
+});
 
  class SimpleTable extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {
-            prod: []
-        }
+      super(props);
+      this.state = {
+          prod: []
+      }
 
-        this.delete = this.delete.bind(this);
-        this.cargaProductos = this.cargaProductos.bind(this);
+      this.delete = this.delete.bind(this);
+      this.cargaProductos = this.cargaProductos.bind(this);
     }
 
     componentDidMount() { 
@@ -38,25 +38,25 @@ const styles = theme => ({
     }
 
     async delete(id) {
-        const respuesta = await fetch(`http://localhost:3000/products/${id}`, {
-          method: 'DELETE',
-        })
-        if(respuesta){
-          console.log('la respuesta luego de borrar en la db es: '+respuesta)
-          console.log('ya se podria ejecutar el carga productos de nuevo al borrar')
-          await alert('Se borro el producto')
-          this.cargaProductos();
-          console.log('supuestamente se ejecuta')
-        } else {
-          alert("Error al borrar")
-        }
-      
-        // if(respuesta.error){
-        //   alert("Error al Borrar el producto!");
-        // } else {
-        //   alert("El producto ha sido borrado");
-        // }
-      // .catch(err => console.log(err)) 
+      const respuesta = await fetch(`http://localhost:3000/products/${id}`, {
+        method: 'DELETE',
+      })
+      if(respuesta){
+        console.log('la respuesta luego de borrar en la db es: '+respuesta)
+        console.log('ya se podria ejecutar el carga productos de nuevo al borrar')
+        await alert('Se borro el producto')
+        this.cargaProductos();
+        console.log('supuestamente se ejecuta')
+      } else {
+        alert("Error al borrar")
+      }
+    
+      // if(respuesta.error){
+      //   alert("Error al Borrar el producto!");
+      // } else {
+      //   alert("El producto ha sido borrado");
+      // }
+    // .catch(err => console.log(err)) 
     }
 
     cargaProductos(){
@@ -73,56 +73,64 @@ const styles = theme => ({
     }
     
     render(){
-        const { classes } = this.props;
+      const { classes } = this.props;
+      const prod = this.state.prod.length;
+      console.log('prods: '+this.state.prod.length)
+      {if(prod){
         return (
-            <TableContainer component={Paper}>
-              <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell align="center">Link</TableCell>
-                    <TableCell align="center">Id</TableCell>
-                    <TableCell align="center">Nombre</TableCell>
-                    <TableCell align="center">Precio</TableCell>
-                    <TableCell align="center">Cantidad</TableCell>
-                    <TableCell align="center">Image</TableCell>
-                    <TableCell align="center">Modificar</TableCell>
-                    <TableCell align="center">Borrar</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {this.state.prod && this.state.prod.map((p) => (
-                    <TableRow key={p.id}>
-                      <TableCell component="th" scope="row" align="center">
-                        <a href={p.linkMeli} target="blank">Ver articulo</a>
-                      </TableCell>
-                      <TableCell align="center">{p.idML}</TableCell>
-                      <TableCell align="center">{p.title}</TableCell>
-                      <TableCell align="center">{p.price}</TableCell>
-                      <TableCell align="center">{p.quantity}</TableCell>
-                      <TableCell align="center">                   
-                        <img src={p.images} style={image} alt=""/>
-                      </TableCell>
-
-                      
-                      <TableCell align="center"> 
-                        <Link to={`/product/modificar/${p.id}`}> 
-                          <Button variant="contained" size="small" color="primary" >
-                            Modificar
-                          </Button>
-                        </Link>
-                      </TableCell>
-                      <TableCell align="center"> 
-                        <Button variant="contained" size="small" color="secondary" onClick={() => this.delete(p.id)} >
-                          Borrar
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Link</TableCell>
+                  <TableCell align="center">Id</TableCell>
+                  <TableCell align="center">Nombre</TableCell>
+                  <TableCell align="center">Precio</TableCell>
+                  <TableCell align="center">Cantidad</TableCell>
+                  <TableCell align="center">Image</TableCell>
+                  <TableCell align="center">Modificar</TableCell>
+                  <TableCell align="center">Borrar</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.prod && this.state.prod.map((p) => (
+                  <TableRow key={p.id}>
+                    <TableCell component="th" scope="row" align="center">
+                      <a href={p.linkMeli} target="blank">Ver articulo</a>
+                    </TableCell>
+                    <TableCell align="center">{p.idML}</TableCell>
+                    <TableCell align="center">{p.title}</TableCell>
+                    <TableCell align="center">{p.price}</TableCell>
+                    <TableCell align="center">{p.quantity}</TableCell>
+                    <TableCell align="center">                   
+                      <img src={p.images} style={image} alt=""/>
+                    </TableCell>
+                    <TableCell align="center"> 
+                      <Link to={`/product/modificar/${p.id}`}> 
+                        <Button variant="contained" size="small" color="primary" >
+                          Modificar
                         </Button>
-                      </TableCell>
-                      
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          )
+                      </Link>
+                    </TableCell>
+                    <TableCell align="center"> 
+                      <Button variant="contained" size="small" color="secondary" onClick={() => this.delete(p.id)} >
+                        Borrar
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          )} else {
+            return(
+            <div className="alert alert-primary" role="alert">
+              No hay productos para
+          </div>
+            ) 
+          }
+
+      }         
     }
 }
 
